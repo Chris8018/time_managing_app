d88c069c39ba4abcee57e17bcd4d1d593e938235
Chris <tranuytrieuvi@gmail.com>
2019-04-20 19:01:02 +0100

    Finalizing Timer Adapter

--- app/src/main/java/com/example/timemanagingapp/MainActivity.java
+++ app/src/main/java/com/example/timemanagingapp/MainActivity.java
@@ -7,17 +7,10 @@ package com.example.timemanagingapp;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // TODO: Replace list with database
+        // TODO: Implement adding timer activity
 
         List<TimerInfo> timers = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            timers.add(new TimerInfo("Task " + i, Integer.toString(i)));
+        }
 
         // Initialize posts
 
@@ -43,35 +41,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new TimerAdapter(timers));
 
-//        FloatingActionButton fab = findViewById(R.id.add_timer_button);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
--- app/src/main/java/com/example/timemanagingapp/TimerAdapter.java
+++ app/src/main/java/com/example/timemanagingapp/TimerAdapter.java
@@ -3,6 +3,8 @@ package com.example.timemanagingapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -30,7 +32,9 @@ public class TimerAdapter extends RecyclerView.Adapter<TimerAdapter.TimerHolder>
 
     @Override
     public void onBindViewHolder(@NonNull TimerAdapter.TimerHolder holder, int position) {
-
+        TimerInfo timer = timers.get(position);
+        holder.task_name.setText(timer.getTask());
+        holder.duration.setText(timer.getDuration());
     }
 
     @Override
@@ -39,9 +43,21 @@ public class TimerAdapter extends RecyclerView.Adapter<TimerAdapter.TimerHolder>
     }
 
     class TimerHolder extends RecyclerView.ViewHolder {
-        //
+
+        private TextView task_name;
+        private TextClock duration;
+        private TextView delete_button;
+        private TextView start_button;
+
+        // TODO: Add click, hold, swipe left, right events for this view holder
         public TimerHolder(@NonNull View itemView) {
             super(itemView);
+            task_name = itemView.findViewById(R.id.task_name);
+            duration = itemView.findViewById(R.id.duration);
+            delete_button = itemView.findViewById(R.id.delete_button);
+            start_button = itemView.findViewById(R.id.start_button);
+
+            // TODO: Add click event for button
         }
     }
 
